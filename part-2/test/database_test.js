8a528faac15417550454849e8d55f75739626000
tdunn1248 <tdunn1248@gmail.com>
2017-08-15 19:39:26 -0700

    fixed async tests

--- part-2/test/database_test.js
+++ part-2/test/database_test.js
@@ -3,9 +3,9 @@ const expect = chai.expect
 
 const {listAllGuests, currentAndFutureBookings, roomCapacityAndAvailability} = require('../models/database.js')
 
-describe('listAllGuests()', function(done) {
-  it('should list all guests', function() {
-    return listAllGuests().then(results => {
+describe('listAllGuests()', function() {
+  it('should list all guests', function(done) {
+    listAllGuests().then(results => {
       expect(results.length).to.be.equal(20)
       expect(typeof results).to.eql('object')
       expect(results[0]).to.be.eql({id: 1, name: 'Aurthur Velti', email: 'avelti0@live.com'})
@@ -15,9 +15,9 @@ describe('listAllGuests()', function(done) {
   })
 })
 
-describe('currentAndFutureBookings()', function(done) {
-  it('lists rooms and current availability', function() {
-    return currentAndFutureBookings().then(results => {
+describe('currentAndFutureBookings()', function() {
+  it('lists rooms and current availability', function(done) {
+    currentAndFutureBookings().then(results => {
       expect(results.length).to.be.equal(39)
       expect(typeof results).to.eql('object')
       expect(results[0]).to.deep.include({room_number: '5A', name: 'Billi Coyne'})
@@ -27,9 +27,9 @@ describe('currentAndFutureBookings()', function(done) {
   })
 })
 
-describe('roomCapacityAndAvailability()', function(done) {
-  it('lists the room bookings (room # and guest name)', function() {
-    return roomCapacityAndAvailability().then(results => {
+describe('roomCapacityAndAvailability()', function() {
+  it('lists the room bookings (room # and guest name)', function(done) {
+    roomCapacityAndAvailability().then(results => {
       expect(results.length).to.be.equal(18)
       expect(typeof results).to.eql('object')
       expect(results[1]).to.deep.eql({ room_number: '3F', capacity: 6, availability: 'true' })
